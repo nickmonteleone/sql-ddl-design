0a24d907c478819337518840bd3c2f3787123c77
Victor Kim <vskimjr@gmail.com>
2023-12-05 19:42:25 -0500

    created stars, planets, moons table, queried for first part of exercise. Awaiting code review.

--- outerspace.sql
+++ outerspace.sql
@@ -16,3 +16,40 @@ CREATE TABLE moons (
     planet  VARCHAR(20) REFERENCES planets
 );
 
+
+-- Insert data into tables
+
+INSERT INTO stars (name, temp_kelvin)
+    VALUES
+        ('The Sun', 5800),
+        ('Proxima Centauri', 3042),
+        ('Gliese 876', 3192);
+
+INSERT INTO planets (name, orbital_period_yrs, star)
+    VALUES
+        ('Earth', 1.00, 'The Sun'),
+        ('Mars', 1.882, 'The Sun'),
+        ('Venus', 0.62, 'The Sun'),
+        ('Proxima Centauri b', 0.03, 'Proxima Centauri'),
+        ('Gliese 876 b', 0.236, 'Gliese 876');
+
+INSERT INTO moons (name, planet)
+    VALUES
+        ('The Moon', 'Earth'),
+        ('Phobos', 'Mars'),
+        ('Deimos', 'Mars');
+
+
+-- name of planet, star is revolves around, number of moons
+
+SELECT planets.name, star, COUNT(moons.name) as moons_count
+    FROM planets
+        LEFT JOIN moons
+        ON planets.name = moons.planet
+    GROUP BY planets.name
+    ORDER BY planets.name ASC;
+
+
+
+
+
